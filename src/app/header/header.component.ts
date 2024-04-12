c2d989b5d8f974000f56512e71351c60fa07da9d
RohitKumar773 <kumarrohit35511@gmail.com>
2024-04-12 12:53:51 +0530

    chne

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,14 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
-
-
-  onWindowScroll(){
-    window.scroll(0,0);
+  onWindowScroll() {
+    window.scroll(0, 0);
   }
-
 }
