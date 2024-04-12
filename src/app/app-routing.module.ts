7faeb52ed15269a2c92f51a86157180e7e3c0994
RohitKumar773 <kumarrohit35511@gmail.com>
2024-04-12 12:37:09 +0530

    all components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PersonComponent } from './person/person.component';
+import { TechComponent } from './tech/tech.component';
+import { SpaceComponent } from './space/space.component';
+import { CountryComponent } from './country/country.component';
+import { CompanyComponent } from './company/company.component';
+import { CollegeComponent } from './college/college.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'person', component: PersonComponent },
+  { path: 'techs', component: TechComponent },
+  { path: 'space', component: SpaceComponent },
+  { path: 'country', component: CountryComponent },
+  { path: 'company', component: CompanyComponent },
+  { path: 'college', component: CollegeComponent },
+  { path: '**', component: HomepageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,48 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { CompanyComponent } from './company/company.component';
+import { CountryComponent } from './country/country.component';
+import { PersonComponent } from './person/person.component';
+import { TechComponent } from './tech/tech.component';
+import { SpaceComponent } from './space/space.component';
+import { AboutComponent } from './about/about.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ViewportComponent } from './viewport/viewport.component';
+import { ContainerComponent } from './container/container.component';
+
+// Angular material used
+import { MatButtonModule } from '@angular/material/button';
+import { CollegeComponent } from './college/college.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    CompanyComponent,
+    CountryComponent,
+    PersonComponent,
+    TechComponent,
+    SpaceComponent,
+    AboutComponent,
+    HomepageComponent,
+    ViewportComponent,
+    ContainerComponent,
+    CollegeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgbModule
+    NgbModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
